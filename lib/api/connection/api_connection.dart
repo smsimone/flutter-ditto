1c33bd264fefa3dbf9b16387b3fb4e348106f561
smaso <smsimone65@gmail.com>
2022-06-09 15:10:13 +0200

    removed unused api

--- lib/api/connection/api_connection.dart
+++ lib/api/connection/api_connection.dart
@@ -10,55 +10,6 @@ class ApiConnection {
 
   final _provider = DittoProvider();
 
-  Future<ProjectList> getProjects(List<String> projectIds) async {
-    final response = await _provider.get(
-      '/projects',
-      queryParams: {'projectIds': projectIds},
-    );
-
-    if (response.statusCode == 200) {
-      final json = jsonDecode(response.body);
-      return ProjectList.fromJson(json);
-    } else {
-      throw Exception('Failed to load projects');
-    }
-  }
-
-  Future<List<ProjectName>> getProjectNames() async {
-    final response = await _provider.get('/project-names');
-
-    if (response.statusCode == 200) {
-      return (jsonDecode(response.body) as List<dynamic>)
-          .map((d) => ProjectName.fromJson(d))
-          .toList();
-    }
-
-    return [];
-  }
-
-  Future<Project?> getProjectById(
-    String? projectId, {
-    String? variant,
-    Format? format,
-    ProjectStatus? status,
-  }) async {
-    projectId ??= DittoConfigs().projectId;
-    final response = await _provider.get('/projects/$projectId');
-    if (response.statusCode == 200) {
-      final json =
-          (jsonDecode(response.body) as Map<String, dynamic>)['projects'];
-      assert(json.entries.length == 1);
-
-      final entry = json.entries.first;
-
-      final projectJson = (entry.value as Map)
-        ..putIfAbsent('project_name', () => entry.key);
-
-      return Project.fromJson(projectJson as Map<String, dynamic>);
-    }
-    return null;
-  }
-
   Future<List<Text>?> getStructuredTexts(String? projectId) async {
     projectId ??= DittoConfigs().projectId;
     final response = await _provider.get(
--- lib/api/models/models.dart
+++ lib/api/models/models.dart
@@ -1,10 +1,5 @@
-export 'block.dart';
 export 'converters.dart';
 export 'enums.dart';
-export 'frame.dart';
 export 'language.dart';
 export 'plurals.dart';
-export 'project.dart';
-export 'project_list.dart';
-export 'project_name.dart';
 export 'text.dart';
